7b32ab2a829d6f465bbd91459d4d9c7d03b78c4c
ericMedeiros93 <littlechico93@gmail.com>
2014-10-08 16:48:19 -0400

    Week 6
    In-class examples for week 6. Review for the Midterm Exam!

--- TipSplit/src/com/ericmedeiros/tipsplit/MainActivity.java
+++ TipSplit/src/com/ericmedeiros/tipsplit/MainActivity.java
@@ -1,10 +1,15 @@
 package com.ericmedeiros.tipsplit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
@@ -13,16 +18,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	Spinner spnTips, spnPeople;
-	EditText edtOtherTip, edtCost;
-	TextView tvTip, tvAmount, tvSplit;
-
+	private Spinner spnTips, spnPeople;
+	private EditText edtOtherTip, edtCost;
+	private TextView tvTip, tvAmount, tvSplit;
+	private double total = 0.0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//Initialize the controls
+
+		// Initialize the controls
 		spnTips = (Spinner) findViewById(R.id.spnTipAmounts);
 		spnPeople = (Spinner) findViewById(R.id.spnPeopleAmount);
 		edtOtherTip = (EditText) findViewById(R.id.edtOtherAmount);
@@ -32,10 +38,10 @@ public class MainActivity extends Activity {
 		tvSplit = (TextView) findViewById(R.id.tvSplit);
 		edtOtherTip.setEnabled(false);
 
-		//Fill the spinners with content
+		// Fill the spinners with content
 		fillSpinners();
 
-		//Create an onItemSelected listener to enable the other EditText
+		// Create an onItemSelected listener to enable the other EditText
 		spnTips.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			@Override
@@ -58,6 +64,31 @@ public class MainActivity extends Activity {
 			}
 
 		});
+
+		spnPeople.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				if (!spnPeople.getSelectedItem().toString().equals("1")) {
+					if (total != 0) {
+						double split = total
+								/ (Double.parseDouble(spnPeople
+										.getSelectedItem().toString()));
+						tvSplit.setText(String
+								.format("$%.2f Per Person", split));
+					}
+				} else {
+					tvSplit.setText("");
+				}
+
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {}
+
+		});
 	}
 
 	@Override
@@ -74,7 +105,7 @@ public class MainActivity extends Activity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			return true;
+			makeDialog("TipSplit", "An easy to use Tip calulator!", true);
 		}
 		return super.onOptionsItemSelected(item);
 	}
@@ -82,7 +113,7 @@ public class MainActivity extends Activity {
 	/**
 	 * Fill the spinners with the arrays set in strings.xml
 	 */
-	public void fillSpinners() {
+	private void fillSpinners() {
 		ArrayAdapter<CharSequence> tipAdapter = ArrayAdapter
 				.createFromResource(this, R.array.tip,
 						android.R.layout.simple_spinner_item);
@@ -100,49 +131,140 @@ public class MainActivity extends Activity {
 
 	/**
 	 * Calculates the totals for tip and per person then displays it
+	 * 
 	 * @param view
 	 */
 	public void calculate(View view) {
 
-		double tipAmountPercent = 0;
-		double tipAmountDollar = 0;
-		double tip = 0;
-		double cost = Double.parseDouble(edtCost.getText().toString());
-		boolean dollar = false;
-		switch (spnTips.getSelectedItemPosition()) {
+		if (edtCost.getText().toString().length() > 0) {
+			double tipAmountPercent = 0;
+			double tipAmountDollar = 0;
+			double tip = 0;
+			double cost = Double.parseDouble(edtCost.getText().toString());
+			boolean dollar = false;
 
-		case 0:
-			tipAmountPercent = .1;
-			break;
-		case 1:
-			tipAmountPercent = .15;
-			break;
-		case 2:
-			tipAmountPercent = .20;
-			break;
-		case 3:
-			double amount = Double.parseDouble(edtOtherTip.getText().toString());
-			tipAmountPercent = (amount / 100);
-			break;
-		case 4:
-			tipAmountDollar = Double.parseDouble(edtOtherTip.getText().toString());
-			dollar = true;
-			break;
+			switch (spnTips.getSelectedItemPosition()) {
+			case 0:
+				tipAmountPercent = .1;
+				break;
+			case 1:
+				tipAmountPercent = .15;
+				break;
+			case 2:
+				tipAmountPercent = .20;
+				break;
+			case 3:
+				if (edtOtherTip.getText().toString().length() > 0) {
+					double amount = Double.parseDouble(edtOtherTip.getText()
+							.toString());
+					tipAmountPercent = (amount / 100);
+				} else {
 
-		}
+					makeDialog("", "You must enter a tip amount!", false);
+					return;
+				}
 
-		if (dollar == true) {
-			tvTip.setText("Tip is: $" + tipAmountDollar);
+				break;
+			case 4:
+				if (edtOtherTip.getText().toString().length() > 0) {
+					tipAmountDollar = Double.parseDouble(edtOtherTip.getText()
+							.toString());
+					dollar = true;
+					break;
+				} else {
+					makeDialog("", "You must enter a tip amount!", false);
+					return;
+				}
+
+			}
+
+			if (dollar == true) {
+				tvTip.setText("Tip is: $" + tipAmountDollar);
+				tip = tipAmountDollar;
+			} else {
+				tip = tipAmountPercent * cost;
+				tvTip.setText(String.format("Tip is: $%.2f ", tip));
+			}
+			total = tip + cost;
+			tvAmount.setText(String.format("Total is: $%.2f ", total));
+
+			if (!spnPeople.getSelectedItem().toString().equals("1")) {
+				double split = total
+						/ (Double.parseDouble(spnPeople.getSelectedItem()
+								.toString()));
+				tvSplit.setText(String.format("$%.2f Per Person", split));
+			}
+
 		} else {
-			tip = tipAmountPercent * cost;
-			tvTip.setText(String.format("Tip is: $%.2f ", tip));
+			makeDialog("", "You must enter the amount you owe!", false);
 		}
-		double total = tip + cost;
-		tvAmount.setText(String.format("Total is: $%.2f ", total));
 
-		double split = total
-				/ (Double.parseDouble(spnPeople.getSelectedItem().toString()));
-		tvSplit.setText(String.format("$%.2f Per Person", split));
+	}
+
+	/**
+	 * Clears all of the controls to the defaults.
+	 * @param view
+	 */
+	public void clear(View view) {
+		spnTips.setSelection(0);
+		spnPeople.setSelection(0);
+		edtOtherTip.setText("");
+		edtCost.setText("");
+		tvTip.setText("");
+		tvAmount.setText("");
+		tvSplit.setText("");
+		edtOtherTip.setText("");
+	}
+
+	@Override
+	/**
+	 * Gets any data that was in the text views showing the totals
+	 */
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		tvTip.setText(savedInstanceState.getString("tvTip"));
+		tvAmount.setText(savedInstanceState.getString("tvAmount"));
+		tvSplit.setText(savedInstanceState.getString("tvSplit"));
+	}
+
+	@Override
+	/**
+	 * Puts any data that was in the text views showing the totals
+	 */
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString("tvTip", tvTip.getText().toString());
+		outState.putString("tvAmount", tvAmount.getText().toString());
+		outState.putString("tvSplit", tvSplit.getText().toString());
+		
+	}
+
+	/**
+	 * This method creates dialog boxes throughout the app.
+	 * @param title Title of the dialog box. Not required.
+	 * @param message Main message of the dialog box.
+	 * @param logo Adds the app logo to the dialog. Used for the about dialog.
+	 */
+	private void makeDialog(String title, String message, boolean logo) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		if (title.length() > 0) {
+			builder.setTitle(title);
+		}
+		builder.setMessage(message);
+		builder.setPositiveButton("Ok", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+			}
+		});
+		if (logo) {
+			builder.setIcon(R.drawable.ic_launcher);
+		}
+		Dialog dialog = builder.create();
+
+		dialog.show();
 
 	}
 
